23b146da88eb66845bae15735d523797ab83d220
bazouga3000 <145032722+lisa56530@users.noreply.github.com>
2024-11-13 14:45:00 +0100

    modèle en place

--- app/src/main/java/com/example/application_de_lisa/Model.kt
+++ app/src/main/java/com/example/application_de_lisa/Model.kt
@@ -250,6 +250,27 @@ data class Cast(
     val profile_path: String? = ""
 )
 
+data class collections(
+    val page: Int,
+    val results: List<Result>,
+    val total_pages: Int,
+    val total_results: Int
+)
+
+
+data class Result(
+    val adult: Boolean,
+    val backdrop_path: String,
+    val id: Int,
+    val name: String,
+    val original_language: String,
+    val original_name: String,
+    val overview: String,
+    val poster_path: String
+)
+ 
+
+
 
 
 
